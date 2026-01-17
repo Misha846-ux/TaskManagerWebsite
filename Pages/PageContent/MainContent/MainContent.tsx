9710a8dd0a2995c2e6e0bc809e0695f614847e25
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2026-01-17 17:32:33 +0200

    add List of Projects

--- Pages/PageContent/MainContent/MainContent.tsx
+++ Pages/PageContent/MainContent/MainContent.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Project_Worked from '../../../Project_Worked/Project_Worked'
 import Timer from '../../../Timer/Timer'
+import "./styles/MainPageContent.css"
+import Projects from '../../../Projects/Projects';
 
 export default function MainContent() {
   return (
@@ -9,7 +11,9 @@ export default function MainContent() {
                 <div className="Calendar-YourProject">
                     <div className="Fake_calendar"></div>
                 <Project_Worked/>
-        </div>
+                </div>
+                <Projects/>
+                
     </div>
   )
 }
