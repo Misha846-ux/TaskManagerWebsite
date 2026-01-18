38a4dbce9b81babcd7cba1fa7f440cd90ad425f1
Misha Gladkov <michael@gladkov.com.ua>
2026-01-18 16:55:14 +0100

    Finish Calendar

--- Pages/PageContent/MainContent/MainContent.tsx
+++ Pages/PageContent/MainContent/MainContent.tsx
@@ -3,6 +3,8 @@ import Project_Worked from '../../../Project_Worked/Project_Worked'
 import Timer from '../../../Timer/Timer'
 import { Outlet, useLoaderData } from 'react-router-dom';
 import Calendar from '../../../Сalendar/Calendar';
+import "./styles/MainPageContent.css"
+import TasksCard from '../../../Сalendar/TasksCard/TasksCard';
 
 export default function MainContent() {
 
@@ -12,7 +14,18 @@ export default function MainContent() {
             <Timer/>
         </div>       
         <div className="Calendar-YourProject">
-            <Calendar/>
+            <div className='calendarWorkBox'>
+              <div className='BoxForCalendar'>
+                <Calendar/>
+              </div>
+              <div style={{width: "29%", height: "99%"}}>
+                <TasksCard title='COMPLETED TASKS' count={5}/>
+                <TasksCard title='PENDING TASKS' count={100}/>
+              </div>
+              
+            </div>
+            
+            
             <Outlet/>
         </div>
     </div>
--- Сalendar/DateCell.tsx
+++ Сalendar/DateCell.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import "./Styles/DateCell.css"
-export default function DateCell(obj: {date: number}) {
+import type { dateCellType } from './Types/DateCellType'
+
+
+export default function DateCell(obj: {day: dateCellType, today: number}) {
+  
   return (
-    <div className='dateCell'>
-      {obj.date}
+    <div className='dateCell' style={{backgroundColor: obj.day.date == obj.today ? "orange" : "white"}}>
+      {obj.day.date}
     </div>
   )
 }
