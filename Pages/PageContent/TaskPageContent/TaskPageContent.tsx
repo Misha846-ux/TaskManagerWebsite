e707ba39a0905097df1affc0631f093a71e8475d
unknown <danakutumov@gmail.com>
2026-01-21 00:34:57 +0200

    created_tasks

--- Pages/PageContent/TaskPageContent/TaskPageContent.tsx
+++ Pages/PageContent/TaskPageContent/TaskPageContent.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import PlacerForTasksComp from './components/PlacerForTasksComp'
+import TaskCreatorComp from './components/TaskCreatorComp'
+import './styles/TaskPageContent.css'
+// import TasksSortComp from './components/TasksSortComp'
 
 export default function TaskPageContent() {
   return (
     <div>
-      
+      <TaskCreatorComp />
+      <PlacerForTasksComp />
     </div>
   )
 }
