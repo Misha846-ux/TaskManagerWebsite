2397d210ffd05b45d43b446999a0b7bd40f6f46d
Misha846-ux <michael@gladkov.com.ua>
2026-01-21 09:08:57 +0100

    Finishing merge

---
+++ Pages/PageContent/TaskPageContent/components/PlacerForTasksComp.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import type { TaskType } from '../types/TaskType';
+import TaskComponent from './TaskComponent';
+import { useLoaderData } from 'react-router-dom';
+
+export default function PlacerForTasksComp() {
+  const tasksData = useLoaderData() as TaskType[] | undefined;
+
+  if (!tasksData) return null;
+
+  return (
+    <div className="placerForTasks">
+      {tasksData.map((task) => (
+        <TaskComponent key={task.id} {...task} />
+      ))}
+    </div>
+  )
+};
